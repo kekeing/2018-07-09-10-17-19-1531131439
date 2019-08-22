cebc35c5f08fc72b4e4e66d6cbf4986039d60720
kekeing <851575436@qq.com>
2019-08-22 21:31:47 +0800

    third commit

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,15 @@
 package practice04;
 
-public class Worker {
+import practice03.Person;
+
+public class Worker extends Person{
+
+	public Worker(String name, int age) {
+		super(name, age);
+	}
+	
+	@Override
+	public String introduce() {
+		return "My name is " + getName()+ ". I am " + getAge() + " years old. I am a Worker. I have a job.";
+}
 }
