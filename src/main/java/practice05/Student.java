bc0191bfe07e82ae68c16a664b6ec8413eae0264
kekeing <851575436@qq.com>
2019-08-22 22:16:50 +0800

    fifth commit

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,27 @@
 package practice05;
 
-public class Student {
+import practice02.Person;
+
+public class Student extends Person{
+	private int klass;
+
+	public int getKlass() {
+		return klass;
+	}
+
+	public void setKlass(int klass) {
+		this.klass = klass;
+	}
+
+	public Student(String name, int age, int klass) {
+		super(name, age);
+		this.klass = klass;
+	}
+	@Override
+	public String introduce() {
+		return "My name is "+getName()+". I am "+getAge()+" years old. I am a Student. I am at Class "+getKlass()+".";
+		
+	}
+	
+	
 }
