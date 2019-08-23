75c60725528c83edada8f0521318d5fd32c21ac5
kekeing <851575436@qq.com>
2019-08-23 10:37:49 +0800

    seventh commit

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,34 @@
 package practice07;
 
-public class Student {
+import practice02.Person;
+
+public class Student extends Person{
+	private Klass klass;
+
+
+public Klass getKlass() {
+		return klass;
+	}
+
+
+	public void setKlass(Klass klass) {
+		this.klass = klass;
+	}
+	
+
+
+public Student(String name, int age, Klass klass) {
+		super(name, age);
+		this.klass = klass;
+	}
+
+
+@Override
+	public String introduce() {
+	int classNumber = getKlass().getNumber() ;
+		return "My name is " + getName() + ". I am " + getAge()+" years old. I am a Student. I am at Class "+classNumber+".";
+		
+	}
+
+	
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,55 @@
 package practice07;
 
-public class Teacher {
+
+
+public class Teacher extends Person{
+	private Klass klass;
+	
+	public Klass getKlass() {
+		return klass;
+	}
+
+
+
+	public void setKlass(Klass klass) {
+		this.klass = klass;
+	}
+
+
+
+
+
+	public Teacher(String name, int age, Klass klass) {
+		super(name, age);
+		this.klass = klass;
+	}
+	
+
+
+
+	public Teacher(String name, int age) {
+		super(name, age);
+	}
+
+
+
+	@Override
+		public String introduce() {
+		if(getKlass() != null)
+		{int classNumber = getKlass().getNumber() ;
+			return "My name is " + getName() + ". I am " + getAge()+" years old. I am a Teacher. I teach Class "+classNumber+".";
+		}	
+		else return "My name is " + getName() + ". I am " + getAge()+" years old. I am a Teacher. I teach No Class.";
+		}
+	public String introduceWith(Student student) {
+		if (student.getKlass() == getKlass()) {
+			return "My name is " + getName() + ". I am " + getAge()+" years old. I am a Teacher. I teach "+student.getName()+".";
+
+		}
+		else return "My name is " + getName() + ". I am " + getAge()+" years old. I am a Teacher. I don't teach "+student.getName()+".";
+
+		
+	}
+	
+
 }
